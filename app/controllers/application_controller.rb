6cce1c5f5ed4c8826452ef35ede03567cb517203
Fred <fredy799@gmail.com>
2016-08-18 12:32:41 +0530

    validations-micropost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "Welcome.. This is a Toy App"
+    render html: "Hi.. This is a Toy App"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :email, length: { minimum: 5 }
+  validates :content, length: { minimum:2 }
 end
